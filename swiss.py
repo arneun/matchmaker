d0b6d3f434d6034268456907f1695e9df68b13fa
Arneun <arneun@Karmagedon.home>
2018-09-26 15:00:35 +0200

    Refractor to readable code

--- swiss.py
+++ swiss.py
@@ -8,78 +8,51 @@ class Swiss(Contest):
     def __init__(self, player_names):
         self.players = []
         self.matches = []
-        for name in player_names:
-            self.players.append(Player(name))
-
-        shuffle(self.players)
-        stack_players = []
         
-        for player in self.players:
-            stack_players.append(player)
-            if(len(stack_players) is 2):
-                    self.matches.append(Match(stack_players.pop(), stack_players.pop()))
+        register_players()
+        
+        shuffle(self.players)
+        
+        matches = assign_matches(players)
 
     def get_round(self):
         return self.matches
 
-    def win_match(self, number, winner):
-        self.matches[number].finish(winner)
-
-    def print_players(self):
-        for player in self.players:
-            print("{} {}".format(player.get_name(), player.get_points()) )
-
+    
     def next_round(self):
-        new_match_table = [] 
-        winners = []
-        for match in self.matches:
-            if(not match.ended):
-                players = match.get_players()
-                raise Not_Finished_Error("Match between " + players[0] + ", and " + players[1] + "is not ended" )
-            else:
-                match.get_winner().winner()
+        try:
+            check_finished_matches()
+        except Not_Finished_Error:
+            return False
         
+        table = give_points()
+        
+        self.matches = assign_matches(table)
+        return True
+
+    
+    
+    
+    def sort_players(self, table):
+        players = []
+        
+        for key in sorted(table.keys()):
+            players = players + table[key]
+        
+        return players
+
+
+    def give_points(self):
         table = {}
-        
         for player in self.players:
             if player.get_points() in table:
                 table[player.get_points()].append(player)
             else: 
                 table[player.get_points()] = [player]
-        
-        new_matches = []
-        for key in sorted(table.keys()):
-            new_matches = new_matches + table[key]
-
-        new_match_table = []
-        stack_players= []
-
-        for player in new_matches:
-            stack_players.append(player)
-            if(len(stack_players) is 2):
-                new_match_table.append(Match(stack_players.pop(), stack_players.pop()))
-
-        if len(stack_players) is not 0:
-            print("Lonely wolfs")
-            for player in stack_players:
-                print(player.get_name())
-
-        self.matches = new_match_table
-        
-        
-        return True
-
-    def print_matches(self):
-        for idx,match in enumerate(self.matches):
-            print("[{}]".format(idx))
-            for player in match.get_players():
-                if match.ended and player.get_name() == match.get_winner().get_name():
-                    print(player.get_name() + "(W)")
-                else:
-                    print(player.get_name())
-            print("")
+        return sort_players(table)
 
 
+   
 
 
 
--- tournament.py
+++ tournament.py
@@ -7,35 +7,29 @@ class Tournament(Contest):
 
     def __init__(self, player_names):
         self.players = []
-        self.matches = []
-        for name in player_names:
-            self.players.append(Player(name))
-
-        shuffle(self.players)
-        stack_players = []
+            
+        register_players()
         
-        for player in self.players:
-            stack_players.append(player)
-            if(len(stack_players) is 2):
-                    self.matches.append(Match(stack_players.pop(), stack_players.pop()))
+        shuffle(self.players)
+        
+        self.matches = assign_matches(players)
 
     def get_round(self):
         return self.matches
 
     def next_round(self):
-        new_match_table = [] 
+        try:
+            check_finished_matches()
+        except Not_Finished_Error:
+            return False
+        
+        self.matches = assign_matches(self.get_winners())
+        
+        return True
+    
+    def get_winners(self):
         winners = []
         for match in self.matches:
-            if(not match.ended):
-                players = match.get_players()
-                raise Not_Finished_Error("Match between " + players[0] + ", and " + players[1] + "is not ended" )
-            else:
-                winners.append(match.get_winner())
-                if(len(winners) is 2):
-                    new_match_table.append(Match(winners.pop(),winners.pop()))
-        self.matches = new_match_table
-        return True
-
-
-           
+            winners.append(match.get_winner())
+        return winners
 
